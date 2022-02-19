41b69dd8e0ff4821a6be9c0f3567de3b2eb97b4e
srakache <100033065+srakache@users.noreply.github.com>
2022-02-19 11:06:16 -0600

    Update backend.tf

--- lab-1-terraform-resources/terraform/resource-group/backend.tf
+++ lab-1-terraform-resources/terraform/resource-group/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  =
-    storage_account_name = 
-    container_name       =
-    key                  = 
+    resource_group_name  ="github-actions-tfstate"
+    storage_account_name = "skgithubactionstfstate"
+    container_name       ="tfstatedevops"
+    key                  = "resourcegroup.tfstate"
   }
 }
 
